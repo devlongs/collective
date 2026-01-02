74aba0aa452a8e2c186f8a36ea8c53d8fe8a0e1e
Longs Pemun Gotar <developerlongs@gmail.com>
2026-01-02 14:31:28 +0100

    feat: add database configuration and connection setup with environment variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/devlongs/collective/internal/db"
 	"github.com/devlongs/collective/internal/env"
 	"github.com/devlongs/collective/internal/store"
 )
@@ -10,9 +11,24 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			dsn:          env.GetString("DB_DSN", "postgres://admin:adminpassword@localhost/collective?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "10m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(cfg.db.dsn, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+
+	log.Println("DB connected succeessfully")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		storage: store,
